4a6d1714a897a8f54221029e8f7139ef9620aa33
KF7EEL <kf7eel@qsl.net>
2022-10-03 12:34:05 -0700

    fix typo

--- bridge.py
+++ bridge.py
@@ -501,7 +501,6 @@ def all_unit_mmdvm_peer(_data):
     for system in CONFIG['SYSTEMS']:
         if CONFIG['SYSTEMS'][system]['ENABLED']:
                 if CONFIG['SYSTEMS'][system]['MODE'] == 'PEER':
-                    if CONFIG['SYSTEMS'][system]['MODE']
                     if 'ALL_MMDVM_UNIT' in CONFIG['SYSTEMS'][system]['OPTIONS']:
                         systems[system].send_system(_data)
 
